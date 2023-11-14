aa37c0d8eb6729b85e79a25ff1f8b505ebe304e5
David Ferneding <fernedingd@gmail.com>
2023-11-14 23:47:50 +0100

    feat: add problem eleven

---
+++ src/problems/eleven.clj
@@ -0,0 +1,146 @@
+; Task: Find the highest product of four adjacent numbers in the given grid
+(ns problems.eleven)
+
+(def grid [[ 8  2 22 97 38 15  0 40  0 75  4  5  7 78 52 12 50 77 91  8]
+           [49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48  4 56 62  0]
+           [81 49 31 73 55 79 14 29 93 71 40 67 53 88 30  3 49 13 36 65]
+           [52 70 95 23  4 60 11 42 69 24 68 56  1 32 56 71 37  2 36 91]
+           [22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80]
+           [24 47 32 60 99  3 45  2 44 75 33 53 78 36 84 20 35 17 12 50]
+           [32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70]
+           [67 26 20 68  2 62 12 20 95 63 94 39 63  8 40 91 66 49 94 21]
+           [24 55 58  5 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72]
+           [21 36 23  9 75  0 76 44 20 45 35 14  0 61 33 97 34 31 33 95]
+           [78 17 53 28 22 75 31 67 15 94  3 80  4 62 16 14  9 53 56 92]
+           [16 39  5 42 96 35 31 47 55 58 88 24  0 17 54 24 36 29 85 57]
+           [86 56  0 48 35 71 89  7  5 44 44 37 44 60 21 58 51 54 17 58]
+           [19 80 81 68  5 94 47 69 28 73 92 13 86 52 17 77  4 89 55 40]
+           [ 4 52  8 83 97 35 99 16  7 97 57 32 16 26 26 79 33 27 98 66]
+           [88 36 68 87 57 62 20 72  3 46 33 67 46 55 12 32 63 93 53 69]
+           [ 4 42 16 73 38 25 39 11 24 94 72 18  8 46 29 32 40 62 76 36]
+           [20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74  4 36 16]
+           [20 73 35 29 78 31 90  1 74 31 49 71 48 86 81 16 23 57  5 54]
+           [ 1 70 54 71 83 51 54 69 16 92 33 48 61 43 52  1 89 19 67 48]])
+(def count-nums 4)
+
+(defn get-highest-product
+  [num-numbers sequence]
+  (loop [pos     0
+         highest -1]
+    (if (> (+ pos num-numbers) (count sequence))
+      highest
+      (let [product (reduce * (take num-numbers (drop pos sequence)))]
+        (if (> product highest)
+          (recur (+ 1 pos) product)
+          (recur (+ 1 pos) highest))))))
+
+(defn get-horizontal
+  [length row start-col grid]
+  (loop [col start-col
+         set []]
+    (let [value (nth (nth grid row) col)]
+      (if (nil? value)
+        nil
+        (if (= (inc (count set)) length)
+          (conj set value)
+          (recur (inc col) (conj set value)))))))
+
+(defn get-horizontal-sets
+  [grid length]
+  (loop [row       0
+         start-col 0
+         results   []]
+    (if (>= row (count grid))
+      results
+      (if (> (+ start-col length) (count (first grid)))
+        (recur (inc row) 0 results)
+        (recur row (inc start-col) (conj results (get-horizontal length row start-col grid)))))))
+
+(defn get-vertical
+  [length start-row col grid]
+  (loop [row start-row
+         set []]
+    (let [value (nth (nth grid row) col)]
+      (if (nil? value)
+        nil
+        (if (= (inc (count set)) length)
+          (conj set value)
+          (recur (inc row) (conj set value)))))))
+
+(defn get-vertical-sets
+  [grid length]
+  (loop [start-row 0
+         col       0
+         results   []]
+    (if (>= col (count (first grid)))
+      results
+      (if (> (+ start-row length) (count grid))
+        (recur 0 (inc col) results)
+        (recur (inc start-row) col (conj results (get-vertical length start-row col grid)))))))
+
+(defn get-diagonal-right
+  [length start-row start-col grid]
+ (loop [row start-row
+        col start-col
+        set []]
+  (let [value (nth (nth grid row) col)]
+    (if (nil? value)
+      nil
+      (if (= (inc (count set)) length)
+        (conj set value)
+        (recur (inc row) (inc col) (conj set value)))))))
+
+(defn get-diagonal-right-sets
+  [grid length]
+  (loop [start-row 0
+         start-col 0
+         results   []]
+    (if (> (+ start-row length) (count grid))
+      results
+      (if (> (+ start-col length) (count (first grid)))
+        (recur (inc start-row) 0 results)
+        (recur start-row (inc start-col) (conj results (get-diagonal length start-row start-col grid)))))))
+
+(defn get-diagonal-left
+  [length start-row start-col grid]
+  (loop [row start-row
+         col start-col
+         set []]
+    (let [value (nth (nth grid row) col)]
+      (if (nil? value)
+        nil
+        (if (= (inc (count set)) length)
+          (conj set value)
+          (recur (inc row) (dec col) (conj set value)))))))
+
+(defn get-diagonal-left-sets
+  [grid length]
+  (loop [start-row 0
+         start-col (dec (count (first grid)))
+         results   []]
+    (if (> (+ start-row length) (count grid))
+      results
+      (if (< (- (inc start-col) length) 0)
+        (recur (inc start-row) (dec (count (first grid))) results)
+        (recur start-row (dec start-col) (conj results (get-diagonal-left length start-row start-col grid)))))))
+
+(comment
+  (def test-data [[1 1]
+                  [2 2]
+                  [3 3]
+                  [4 4]])
+
+  (concat [0 0] (first (rest test-data)))
+
+  (get-vertical 2 2 0 test-data)
+
+  (apply max (map #(apply * %) (get-horizontal-sets test-data 2)))
+  (apply max (map #(apply * %) (get-vertical-sets test-data 2)))
+  (apply max (map #(apply * %) (get-diagonal-right-sets test-data 2)))
+  (apply max (map #(apply * %) (get-diagonal-left-sets test-data 2)))
+
+  (max (apply max (map #(apply * %) (get-horizontal-sets grid count-nums)))
+       (apply max (map #(apply * %) (get-vertical-sets grid count-nums)))
+       (apply max (map #(apply * %) (get-diagonal-right-sets grid count-nums)))
+       (apply max (map #(apply * %) (get-diagonal-left-sets grid count-nums))))
+  :rcf)
