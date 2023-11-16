9eb4698dc0b7889866ae676cefe47b47031f20be
David Ferneding <fernedingd@gmail.com>
2023-11-16 21:44:44 +0100

    feat: add problem sixteen

---
+++ src/problems/sixteen.clj
@@ -0,0 +1,22 @@
+; Task: Find the sum of all digits of 2^1000
+(ns problems.sixteen)
+
+(defn digits
+  [number]
+  (loop [number-string (str (biginteger number))
+         digits []]
+    (let [digit (first number-string)]
+     (if (nil? digit)
+       digits
+       (let [digit (- (byte digit) 48)]
+         (recur (rest number-string) (conj digits digit)))))))
+
+(defn sum-digits
+  [number]
+  (reduce + (digits number)))
+
+(comment
+  (digits 123)
+  (digits (.pow (biginteger 2) 1000))
+  (sum-digits (.pow (biginteger 2) 1000))
+  :rcf)
